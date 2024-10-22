934f8ddd9a97b23fec76ebfcb2a50665b6e6ac2e
YLG-dev <youenn.legouedec@gmail.com>
2024-10-23 00:43:40 +0200

    test

--- app/Jobs/ProcessWebhookJob.php
+++ app/Jobs/ProcessWebhookJob.php
@@ -9,10 +9,6 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob
 {
     public function handle()
     {
-        Process::path(__DIR__)->run('git stash');
-        Process::path(__DIR__)->run('git pull origin main');
-        Process::path(__DIR__)->run('php artisan optimize:clear');
-        Process::path(__DIR__)->run('php artisan migrate --force');
-        Process::path(__DIR__)->run('php artisan optimize');
+        Process::path(__DIR__)->run('bash deploy.sh');
     }
 }
