05fcd69eacb6882e4ba2bd3780a85474c88cc1d9
YLG-dev <youenn.legouedec@gmail.com>
2024-10-23 01:52:10 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1030,7 +1030,7 @@
                                         <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
 
                                         <p class="mt-4 text-sm/relaxed">
-                                            Lavavlsyoupipa has wonderful documentation covering every aspect of the
+                                            Lavavlsyoupipas has wonderful documentation covering every aspect of the
                                             frameswovrk.
                                             Whether you are a newcomer or have prior experience with Laravel, we
                                             recommend reading our documentation from beginning to end.
