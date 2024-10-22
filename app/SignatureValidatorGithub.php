d832d4219c65036e60895b6eea1e7c245b66427b
YLG-dev <youenn.legouedec@gmail.com>
2024-10-22 16:05:53 +0200

    test

---
+++ app/SignatureValidatorGithub.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Http\Request;
+use Spatie\WebhookClient\Exceptions\InvalidConfig;
+use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
+use Spatie\WebhookClient\WebhookConfig;
+
+class SignatureValidatorGithub implements SignatureValidator
+{
+    public function isValid(Request $request, WebhookConfig $config): bool
+    {
+        $signature = $request->header($config->signatureHeaderName);
+
+        if (!$signature) {
+            return false;
+        }
+
+        $signingSecret = $config->signingSecret;
+
+        if (empty($signingSecret)) {
+            throw InvalidConfig::signingSecretNotSet();
+        }
+
+        $computedSignature = 'sha256=' . hash_hmac('sha256', $request->getContent(), $signingSecret);
+
+        return hash_equals($computedSignature, $signature);
+    }
+}
